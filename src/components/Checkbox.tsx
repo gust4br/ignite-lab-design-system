afcf26c8e8ca4ef91e0ef74e11a3c2d873d5e710
Gustavo Gomes <gustavosgdev@outlook.com>
2022-10-12 23:54:06 -0400

    feat: interface criada

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,7 +1,7 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-export interface CheckboxProps{
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps{
   
 }
 
@@ -9,7 +9,8 @@ export function Checkbox(props: CheckboxProps){
 
   return (
     <CheckboxPrimitive.Root 
-      className="w-6 h-6 p-[2px] bg-gray-800 rounded" 
+      className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
     >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
